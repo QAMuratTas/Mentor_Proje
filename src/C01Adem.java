3646ae97f0c08af688ae23057fd950690d084d4f
ademalabas@gmail.com <ademalabas@gmail.com>
2022-10-07 10:32:07 +0300

    Branch

--- src/C01Adem.java
+++ src/C01Adem.java
@@ -3,6 +3,8 @@ public class C01Adem {
         System.out.println("Hi to everyne");
         System.out.println("Hi to everyne");
         System.out.println("Hi to everyne");
+        System.out.println("Hi to everyne");
+        System.out.println("Hi to everyne");
 
 
     }
