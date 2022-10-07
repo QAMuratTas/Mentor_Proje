f5e303bf3d0e98c3d8bf875a93c2d7297018bf22
ismail5240 <arkd50279@gmail.com>
2022-10-07 10:40:09 +0300

    ismail branch

--- src/İsmail.java
+++ src/İsmail.java
@@ -2,5 +2,10 @@ public class İsmail {
     public static void main(String[] args) {
 
         System.out.println("ismail'in localin'den Merhaba");
+
+        // deneme
+
+        System.out.println("nazilliden selamlar");
+       /// / selamlar
     }
 }
