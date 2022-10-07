478568942f14d56a71f63c68855ab4615cfd6bfe
AlwaysJusticeForAll <cihatkose2000@gmail.com>
2022-10-07 11:29:16 +0300

    selam

--- src/Cihat.java
+++ src/Cihat.java
@@ -2,6 +2,6 @@ public class Cihat {
     public static void main(String[] args) {
         System.out.println("Cihat'ın lokalinden merhaba");
         System.out.println("Justice");
-        //selamlar
+        //selamlarr
     }
 }
